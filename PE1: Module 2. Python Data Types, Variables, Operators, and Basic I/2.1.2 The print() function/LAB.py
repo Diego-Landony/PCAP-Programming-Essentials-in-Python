4d5c0b7c00a06659f0bb667eb269509670474fbb
Diego Landony Lima Toledo <132611526+Diego-Landony@users.noreply.github.com>
2023-10-07 18:33:36 -0600

    Lab 1

---
+++ PE1: Module 2. Python Data Types, Variables, Operators, and Basic I/2.1.2 The print() function/LAB.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+print("Hello, Python!")
+#print("Diego") "Diego"
+#print(Diego)  "NameError: name 'Diego' is not defined"
+#print"Diego" "SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?"
+#print('Diego') "Diego"
+#print("Diego") print("Python") "SyntaxError: invalid syntax"
+
+
