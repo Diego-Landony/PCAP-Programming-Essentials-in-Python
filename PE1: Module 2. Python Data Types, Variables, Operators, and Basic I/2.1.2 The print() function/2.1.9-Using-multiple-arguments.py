dd24fdb8d020dbd7c9b5f37bca6940cc44825786
Diego Landony Lima Toledo <132611526+Diego-Landony@users.noreply.github.com>
2023-10-08 17:07:03 -0600

    Module 2

---
+++ PE1: Module 2. Python Data Types, Variables, Operators, and Basic I/2.1.2 The print() function/2.1.9-Using-multiple-arguments.py
@@ -0,0 +1,9 @@
+print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
+
+#There is one print() function invocation, but it contains three arguments. All of them are strings.
+#The arguments are separated by commas. We've surrounded them with spaces to make them more visible, 
+#but it's not really necessary, and we won't be doing it anymore.
+
+#Two conclusions emerge from this example:
+#1.a print() function invoked with more than one argument outputs them all on one line;
+#2.the print() function puts a space between the outputted arguments on its own initiative.
