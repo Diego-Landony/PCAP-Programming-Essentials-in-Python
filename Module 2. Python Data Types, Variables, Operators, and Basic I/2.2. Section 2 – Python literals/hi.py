c16ee349934c0088a23031ff5ae0e923c84ea54f
Diego Landony Lima Toledo <132611526+Diego-Landony@users.noreply.github.com>
2023-10-11 18:39:53 -0600

    j

---
+++ Module 2. Python Data Types, Variables, Operators, and Basic I/2.2. Section 2 – Python literals/hi.py
@@ -0,0 +1,7 @@
+from datetime import date
+print(date.today())
+print("Today's date is: " + str(date.today()))
+
+print("Welcome to the greeter program")
+name = input("Enter your name: ")
+print("Greetings " + name)
