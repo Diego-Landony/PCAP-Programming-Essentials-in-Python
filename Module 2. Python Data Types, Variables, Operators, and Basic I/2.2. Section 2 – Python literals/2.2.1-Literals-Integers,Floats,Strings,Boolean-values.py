fb33b1ba68f7c0e3fd9732800feee52f905ddb9f
Diego Landony Lima Toledo <132611526+Diego-Landony@users.noreply.github.com>
2023-10-12 19:08:09 -0600

    literals

---
+++ Module 2. Python Data Types, Variables, Operators, and Basic I/2.2. Section 2 – Python literals/2.2.1-Literals-Integers,Floats,Strings,Boolean-values.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+# Integer Literal
+# An integer literal is a number without a fractional part.
+a = 10  # Decimal
+b = 0b1010  # Binary
+c = 0o12  # Octal
+d = 0xA  # Hexadecimal
+
+print("Integer Literals:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
+print("d =", d)
+
+# Float Literal
+# A float literal is a real number with both an integer and a fractional part.
+e = 3.14  # Decimal
+f = 1.23e4  # Scientific Notation
+
+print("\nFloat Literals:")
+print("e =", e)
+print("f =", f)
+
+# String Literal
+# A string literal is a sequence of characters enclosed in single, double, or triple quotes.
+g = 'Hello'  # Single quotes
+h = "World"  # Double quotes
+i = '''This is an example
+of a multi-line string'''  # Triple quotes
+
+print("\nString Literals:")
+print("g =", g)
+print("h =", h)
+print("i =", i)
+
+# Boolean Literal
+# A boolean literal is a value that represents either truth or falsehood.
+j = True  # True
+k = False  # False
+
+print("\nBoolean Literals:")
+print("j =", j)
+print("k =", k)
+
+# Special Literal
+# A special literal is the value 'None,' which represents the absence of an object.
+l = None  # None
+
+print("\nSpecial Literal:")
+print("l =", l)
